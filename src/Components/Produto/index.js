e65025761cbad8e11f53d87e28d99912a09747f5
soarezlucas02 <lucassoaresesouza@gmail.com>
2023-07-19 19:19:11 -0300

    passando pro git

--- src/Components/Produto/index.js
+++ src/Components/Produto/index.js
@@ -1,10 +1,13 @@
+import { ProdutoDiv } from "./style";
 
-const Produto = () => {
-    
-    return(
-        <div></div>
-    )
-
-}
+const Produto = (props) => {
+  return (
+    <ProdutoDiv>
+      <img alt="imagem" src="./." />
+      <p>{props.nome}</p>
+      <p>{props.quantidade}</p>
+    </ProdutoDiv>
+  );
+};
 
 export default Produto;
