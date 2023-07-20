832c056decc58cd10312ec21bab9493bcab37804
soarezlucas02 <lucassoaresesouza@gmail.com>
2023-07-20 00:33:14 -0300

    aplicando mudancas no header

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import './App.css';
 import { GlobalStyle } from './Constants/Theme/styles'
 import Estoque from './Pages/Estoque';
-import Login from './Pages/Login';
 
 function App() {
   return (
     <div>
       <GlobalStyle/>
-        <Login/>
-      
+        <Estoque/>
 
     </div>
   );
--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -1,13 +1,15 @@
+import { Botao } from "../Botao/style";
+import { Input } from "../Input/style";
 import { HeaderDiv } from "./styles";
 
 const Header = () => {
     return (
         <HeaderDiv>
             <h1>Delicias Caseiras</h1>
-            <input placeholder="Pesquise aqui um sabor"/>
-            <a href="/produtos">Produtos</a>
-            <a href="/encomendas">Encomendas</a>
-            <button>Sair</button>
+            <Input placeholder="Pesquise aqui um sabor"/>
+            <Botao href="/produtos">Produtos</Botao>
+            <Botao href="/encomendas">Encomendas</Botao>
+            <Botao>Sair</Botao>
         </HeaderDiv>
     )
 }
